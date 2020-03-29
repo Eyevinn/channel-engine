ca5b4d4fad9ca17ad66c6d89285c21f4f086401b
birme <jonas.birme@eyevinn.se>
2020-03-29 21:45:46 +0200

    Unit test that reproduces the issue that the same top segment occurs even though the media sequence is increased

--- spec/engine/playhead_spec.js
+++ spec/engine/playhead_spec.js
@@ -1,4 +1,6 @@
 const Session = require('../../engine/session.js');
+const m3u8 = require('@eyevinn/m3u8');
+const Readable = require('stream').Readable;
 
 class TestAssetManager {
   constructor() {
@@ -37,6 +39,47 @@ describe("Playhead consumer", () => {
         if (!manifest.match('#EXT-X-MEDIA-SEQUENCE:' + expectedMseq++)) {
           fail(manifest);
         }
+        currentMediaManifest = await session.getCurrentMediaManifest(180000);
+        expect(currentMediaManifest).toEqual(manifest);
+      }
+    };
+    await loop(100);
+    done();
+  });
+
+  it("never get the same top segment after media sequence is increased", async (done) => {
+    const assetMgr = new TestAssetManager();
+    const session = new Session(assetMgr, { sessionId: '1' });
+    const loop = async (increments) => {
+      let remain = increments;
+      let promiseFns = [];
+      while (remain > 0) {
+        promiseFns.push(() => session.increment());
+        remain--;
+      }
+      let lastUri = null;
+      let lastManifest = null;
+      for (let promiseFn of promiseFns) {
+        manifest = await promiseFn();
+        const parser = m3u8.createStream();
+        await new Promise((resolve, reject) => {
+          let manifestStream = new Readable();
+          manifestStream.push(manifest);
+          manifestStream.push(null);
+  
+          manifestStream.pipe(parser);
+          parser.on('m3u', m3u => {
+            const firstItem = m3u.items.PlaylistItem[0];
+            if (lastUri && firstItem.get('uri') === lastUri) {
+              console.log(lastManifest);
+              console.log(manifest);
+              fail(`${m3u.get('mediaSequence')}:${firstItem.get('uri')} was included in last media sequence (${lastUri})`);
+            }
+            lastUri = firstItem.get('uri');
+            resolve();
+          });
+        });
+        lastManifest = manifest;
       }
     };
     await loop(100);
