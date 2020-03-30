d5a69aa51d178b0aa9a3850ade8f8ed068830925
Jonas Birmé <jonas.birme@eyevinn.se>
2020-03-30 17:33:56 +0200

    Bugfix/three short vods (#6)
    * Verify three short VODs in a row
    
    * Unit test to test three short VODs in a row

--- spec/engine/playhead_spec.js
+++ spec/engine/playhead_spec.js
@@ -3,11 +3,14 @@ const m3u8 = require('@eyevinn/m3u8');
 const Readable = require('stream').Readable;
 
 class TestAssetManager {
-  constructor() {
+  constructor(assets) {
     this.assets = [
       { id: 1, title: "Tears of Steel", uri: "https://maitv-vod.lab.eyevinn.technology/tearsofsteel_4k.mov/master.m3u8" },
       { id: 2, title: "VINN", uri: "https://maitv-vod.lab.eyevinn.technology/VINN.mp4/master.m3u8" }
     ];
+    if (assets) {
+      this.assets = assets;
+    }
     this.pos = 0;
   }
   getNextVod(vodRequest) {
@@ -21,6 +24,39 @@ class TestAssetManager {
 
   }
 }
+
+const verificationLoop = async (session, increments) => {
+  let remain = increments;
+  let promiseFns = [];
+  while (remain > 0) {
+    promiseFns.push(() => session.increment());
+    remain--;
+  }
+  let lastUri = null;
+  let lastManifest = null;
+  for (let promiseFn of promiseFns) {
+    manifest = await promiseFn();
+    const parser = m3u8.createStream();
+    await new Promise((resolve, reject) => {
+      let manifestStream = new Readable();
+      manifestStream.push(manifest);
+      manifestStream.push(null);
+
+      manifestStream.pipe(parser);
+      parser.on('m3u', m3u => {
+        const firstItem = m3u.items.PlaylistItem[0];
+        if (lastUri && firstItem.get('uri') === lastUri) {
+          console.log(lastManifest);
+          console.log(manifest);
+          fail(`${m3u.get('mediaSequence')}:${firstItem.get('uri')} was included in last media sequence (${lastUri})`);
+        }
+        lastUri = firstItem.get('uri');
+        resolve();
+      });
+    });
+    lastManifest = manifest;
+  }
+};
 
 describe("Playhead consumer", () => {
   it("continously increases media sequence over two VOD switches", async (done) => {
@@ -50,39 +86,14 @@ describe("Playhead consumer", () => {
   it("never get the same top segment after media sequence is increased", async (done) => {
     const assetMgr = new TestAssetManager();
     const session = new Session(assetMgr, { sessionId: '1' });
-    const loop = async (increments) => {
-      let remain = increments;
-      let promiseFns = [];
-      while (remain > 0) {
-        promiseFns.push(() => session.increment());
-        remain--;
-      }
-      let lastUri = null;
-      let lastManifest = null;
-      for (let promiseFn of promiseFns) {
-        manifest = await promiseFn();
-        const parser = m3u8.createStream();
-        await new Promise((resolve, reject) => {
-          let manifestStream = new Readable();
-          manifestStream.push(manifest);
-          manifestStream.push(null);
-  
-          manifestStream.pipe(parser);
-          parser.on('m3u', m3u => {
-            const firstItem = m3u.items.PlaylistItem[0];
-            if (lastUri && firstItem.get('uri') === lastUri) {
-              console.log(lastManifest);
-              console.log(manifest);
-              fail(`${m3u.get('mediaSequence')}:${firstItem.get('uri')} was included in last media sequence (${lastUri})`);
-            }
-            lastUri = firstItem.get('uri');
-            resolve();
-          });
-        });
-        lastManifest = manifest;
-      }
-    };
-    await loop(100);
+    await verificationLoop(session, 10);
+    done();
+  });
+
+  it("can handle three short VODs in a row", async (done) => {
+    const assetMgr = new TestAssetManager([{ id: 1, title: "Short", uri: "https://maitv-vod.lab.eyevinn.technology/VINN.mp4/master.m3u8" }]);
+    const session = new Session(assetMgr, { sessionId: '1' });
+    await verificationLoop(session, 10);
     done();
   });
 });
