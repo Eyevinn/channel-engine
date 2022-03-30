75720b86a216920a4717d5e530f73566eed4bf5e
Nicholas Frederiksen <nikko.frederiksen@gmail.com>
2022-03-30 14:41:10 +0200

    test: update for new filter logic

--- spec/engine/util_spec.js
+++ spec/engine/util_spec.js
@@ -2,7 +2,7 @@ const { filterQueryParser, applyFilter } = require('../../engine/util.js');
 
 describe("Filter Query parser", () => {
   it("can handle systemBitrate low/high range", () => {
-    const filterQuery = `(type=="video"&&systemBitrate>100000)&&(type=="video"&&systemBitrate<800000)`;
+    const filterQuery = `(type=="video"ANDsystemBitrate>100000)AND(type=="video"ANDsystemBitrate<800000)`;
     const filter = filterQueryParser(filterQuery);
   
     expect(filter.video.systemBitrate.low).toEqual(100000);
@@ -10,11 +10,18 @@ describe("Filter Query parser", () => {
   });
 
   it("can handle resolution low/high range", () => {
-    const filterQuery = `(type=="video"&&height>600)&&(type=="video"&&height<1080)`;
+    const filterQuery = `(type=="video"ANDheight>600)AND(type=="video"ANDheight<1080)`;
     const filter = filterQueryParser(filterQuery);
 
     expect(filter.video.height.low).toEqual(600);
     expect(filter.video.height.high).toEqual(1080);  
+  });
+
+  it("can handle a faulty filter query", () => {
+    const filterQuery = `type=video&height=600&type=video&height=1080`;
+    const filter = filterQueryParser(filterQuery);
+
+    expect(filter).toEqual({}); 
   });
 });
 
@@ -60,10 +67,68 @@ describe("Profile filter", () => {
 
   
   it ("can filter out video tracks with height min 200 and max 400", () => {
-    const filterQuery = `(type=="video"&&height>200)&&(type=="video"&&height<400)`;
+    const filterQuery = `(type=="video"ANDheight>200)AND(type=="video"ANDheight<400)`;
     const filter = filterQueryParser(filterQuery);
 
     const filteredProfiles = applyFilter(PROFILE, filter);
     expect(filteredProfiles.length).toEqual(2);
   });
+
+  it ("can filter out video tracks with bitrate min 1414000 and max 3232000", () => {
+    const filterQuery = `(type=="video"ANDsystemBitrate>1414000)AND(type=="video"ANDsystemBitrate<3232000)`;
+    const filter = filterQueryParser(filterQuery);
+
+    const filteredProfiles = applyFilter(PROFILE, filter);
+    expect(filteredProfiles.length).toEqual(1);
+    expect(filteredProfiles[0].bw).toEqual(2323000);
+  });
+
+  it ("can filter out video tracks with bitrate min 1414000 and height min 300", () => {
+    const filterQuery = `(type=="video"ANDsystemBitrate>1414000)AND(type=="video"ANDheight>300)`;
+    const filter = filterQueryParser(filterQuery);
+
+    const filteredProfiles = applyFilter(PROFILE, filter);
+    expect(filteredProfiles.length).toEqual(1);
+    expect(filteredProfiles[0].bw).toEqual(6134000);
+  });
+
+  it ("can filter out video tracks with bitrate min 1414000", () => {
+    const filterQuery = `(type=="video"ANDsystemBitrate>1414000)`;
+    const filter = filterQueryParser(filterQuery);
+
+    const filteredProfiles = applyFilter(PROFILE, filter);
+    expect(filteredProfiles.length).toEqual(2);
+    expect(filteredProfiles[1].bw).toEqual(2323000);
+    expect(filteredProfiles[0].bw).toEqual(6134000);
+  });
+
+  it ("can filter out video tracks with bitrate max 1414000", () => {
+    const filterQuery = `(type=="video"ANDsystemBitrate<1414000)`;
+    const filter = filterQueryParser(filterQuery);
+
+    const filteredProfiles = applyFilter(PROFILE, filter);
+    expect(filteredProfiles.length).toEqual(1);
+    expect(filteredProfiles[0].bw).toEqual(1313000);
+  });
+
+  it ("can filter out video tracks with height min 250", () => {
+    const filterQuery = `(type=="video"ANDheight>250)`;
+    const filter = filterQueryParser(filterQuery);
+
+    const filteredProfiles = applyFilter(PROFILE, filter);
+    expect(filteredProfiles.length).toEqual(2);
+    expect(filteredProfiles[1].bw).toEqual(2323000);
+    expect(filteredProfiles[0].bw).toEqual(6134000);
+  });
+
+  it ("can filter out video tracks with height max 300", () => {
+    const filterQuery = `(type=="video"ANDheight<300)`;
+    const filter = filterQueryParser(filterQuery);
+
+    const filteredProfiles = applyFilter(PROFILE, filter);
+    expect(filteredProfiles.length).toEqual(2);
+    expect(filteredProfiles[1].bw).toEqual(1313000);
+    expect(filteredProfiles[0].bw).toEqual(2323000);
+  });
+
 });
