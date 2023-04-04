fae5fe75e4a659f399fc048ede224c10a1adfc97
Nicholas Frederiksen <nicholas.frederiksen@eyevinn.se>
2023-04-04 16:52:41 +0200

    fix: incrementing mediaSeq by 0 not working

--- engine/session_state.js
+++ engine/session_state.js
@@ -116,7 +116,7 @@ class SharedSessionState {
   async increment(key, inc) {
     let value = await this.get(key);
     if (await this.store.isLeader(this.instanceId)) {
-      let valueToIncrement = inc || 1;
+      let valueToIncrement = inc === 0 ? 0 : inc ? inc : 1;
       debug(`[${this.sessionId}]: I am incrementing key ${key} with ${valueToIncrement}`);
       value += valueToIncrement;
       return await this.store.set(this.sessionId, key, value);
