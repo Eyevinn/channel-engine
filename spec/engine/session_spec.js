5089dde9d2b125cf59e42d5e6541f2e4017e5374
Nicholas Frederiksen <nicholas.frederiksen@eyevinn.se>
2023-11-01 09:32:17 +0100

    test: for new increment function

--- spec/engine/session_spec.js
+++ spec/engine/session_spec.js
@@ -18,7 +18,7 @@ describe("Session", () => {
     expect(id1).not.toEqual(id2);
   });
 
-  fit("for demuxed, returns the appropriate audio increment value", async () => {
+  it("for demuxed, returns the appropriate audio increment value when desync is within acceptable limit", async () => {
     const session = new Session("dummy", null, sessionLiveStore);
     const mockFinalAudioIdx = 50; // current Vod has 50 media sequences to serve.
     const mockCurrentVideoPosition = 200.0 * 1000; // Video is 200s deep into its content.
@@ -27,66 +27,70 @@ describe("Session", () => {
       const mockPositions = [196.0, 199.84, 203.68, 207.52];
       return mockPositions[pos_n_current - mockMseqAudio];
     };
-    const output = await session._determineAudioIncrement(
+    const output = await session._determineExtraMediaIncrement(
+      "audio",
       mockCurrentVideoPosition,
       mockFinalAudioIdx,
       mockMseqAudio,
       mock_getAudioPlayheadPosition,
       24
     );
-    expect(output).toBe(1);
+    expect(output.increment).toBe(1);
   });
 
-  fit("for demuxed, returns the appropriate audio increment value", async () => {
+  it("for demuxed, returns the appropriate audio increment value when they are in sync but there is a floating point error", async () => {
     const session = new Session("dummy", null, sessionLiveStore);
-    const mockFinalAudioIdx = 50; // current Vod has 50 media sequences to serve.
-    const mockCurrentVideoPosition = 441.7599999999981697 * 1000; // Video is 200s deep into its content.
-    const mockMseqAudio = 25; // current mseq for audio on vod, 25 out of 50.
+    const mockFinalAudioIdx = 50;
+    const mockCurrentVideoPosition = 441.7599999999981697 * 1000; 
+    const mockMseqAudio = 25;
     const mock_getAudioPlayheadPosition = async (pos_n_current) => {
       const mockPositions = [437.919999999999, 441.75999999999897];
       return mockPositions[pos_n_current - mockMseqAudio];
     };
-    const output = await session._determineAudioIncrement(
+    const output = await session._determineExtraMediaIncrement(
+      "audio",
       mockCurrentVideoPosition,
       mockFinalAudioIdx,
       mockMseqAudio,
       mock_getAudioPlayheadPosition,
       24
     );
-    expect(output).toBe(1);
+    expect(output.increment).toBe(1);
   });
-  fit("for demuxed, returns the appropriate audio increment value", async () => {
+  it("for demuxed, returns the appropriate audio increment value, normal case I", async () => {
     const session = new Session("dummy", null, sessionLiveStore);
-    const mockFinalAudioIdx = 50; // current Vod has 50 media sequences to serve.
-    const mockCurrentVideoPosition = 3.840 * 8 * 1000; // Video is 200s deep into its content.
-    const mockMseqAudio = 5; // current mseq for audio on vod, 25 out of 50.
+    const mockFinalAudioIdx = 50;
+    const mockCurrentVideoPosition = 3.840 * 8 * 1000;
+    const mockMseqAudio = 5;
     const mock_getAudioPlayheadPosition = async (pos_n_current) => {
       const mockPositions = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52];
       return mockPositions[pos_n_current];
     };
-    const output = await session._determineAudioIncrement(
+    const output = await session._determineExtraMediaIncrement(
+      "audio",
       mockCurrentVideoPosition,
       mockFinalAudioIdx,
       mockMseqAudio,
       mock_getAudioPlayheadPosition
     );
-    expect(output).toBe(3);
+    expect(output.increment).toBe(3);
   });
-  fit("for demuxed, returns the appropriate audio increment value", async () => {
+  it("for demuxed, returns the appropriate audio increment value, normal case II", async () => {
     const session = new Session("dummy", null, sessionLiveStore);
-    const mockFinalAudioIdx = 50; // current Vod has 50 media sequences to serve.
-    const mockCurrentVideoPosition = 14 * 1000; // Video is 200s deep into its content.
-    const mockMseqAudio = 0; // current mseq for audio on vod, 25 out of 50.
+    const mockFinalAudioIdx = 50;
+    const mockCurrentVideoPosition = 14 * 1000;
+    const mockMseqAudio = 0;
     const mock_getAudioPlayheadPosition = async (pos_n_current) => {
       const mockPositions = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52];
       return mockPositions[pos_n_current];
     };
-    const output = await session._determineAudioIncrement(
+    const output = await session._determineExtraMediaIncrement(
+      "subtitle",
       mockCurrentVideoPosition,
       mockFinalAudioIdx,
       mockMseqAudio,
       mock_getAudioPlayheadPosition
     );
-    expect(output).toBe(4);
+    expect(output.increment).toBe(4);
   });
 });
